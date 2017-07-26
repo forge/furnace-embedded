d36d8fdf2cc903f547429e0970b72c514dda4573
George Gastaldi <gegastaldi@gmail.com>
2017-07-26 14:22:23 +0000

    Logging NoClassDefFound errors

--- src/main/java/org/jboss/forge/furnace/embedded/EmbeddedFurnaceExtension.java
+++ src/main/java/org/jboss/forge/furnace/embedded/EmbeddedFurnaceExtension.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Observes;
@@ -69,7 +70,7 @@ public class EmbeddedFurnaceExtension implements Extension
                }
                catch (NoClassDefFoundError ncde)
                {
-                  // ignore
+                  log.log(Level.FINE, "Could not load class " + line + ". Ignoring", ncde);
                }
             }
          }
@@ -98,7 +99,7 @@ public class EmbeddedFurnaceExtension implements Extension
                }
                catch (NoClassDefFoundError ncde)
                {
-                  // ignore
+                  log.log(Level.FINE, "Could not load class " + line + ". Ignoring", ncde);
                }
             }
          }
